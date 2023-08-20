5cb5c57e43af3dc2dec445c8e09ed113b693dbbc
woodysee <26299763+woodysee@users.noreply.github.com>
2023-08-20 16:03:59 +0800

    Add anchor links

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # rust-htmx-ssr-template
 
 To run the server in dev mode: `cargo run`
+
+To build a production version: `cargo build --release && ./target/release/rust-htmx-ssr-template`
--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,7 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
-    let get_but_throttled = b"GET /throttled_home HTTP/1.1\r\n";
+    let get_but_throttled = b"GET /htmx-throttled HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
         ("HTTP/1.1 200 OK", "index.html")
