5a1bf173e7792475c1af5471b2750609f52e8e58
woodysee <26299763+woodysee@users.noreply.github.com>
2023-08-20 11:25:02 +0800

    Return static HTML for any unvalidated request

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::io::prelude::*;
+use std::fs;
 
 fn main() {
     // unwrap to panic if there is an error variant
@@ -16,8 +17,10 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
-    println!(
-        "Request: {}",
-        String::from_utf8_lossy(&buffer[..])
-    );
+
+    let contents = fs::read_to_string("index.html").unwrap();
+    
+    let response = format!("HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}", contents.len(), contents);
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
