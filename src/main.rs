3668800530b9cf60239625a82e2f9ffdbd88f6d9
woodysee <26299763+woodysee@users.noreply.github.com>
2023-08-20 11:49:08 +0800

    Add home and 404 page

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
-use std::net::TcpListener;
-use std::net::TcpStream;
-use std::io::prelude::*;
 use std::fs;
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
     // unwrap to panic if there is an error variant
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
         println!("Connection established!");
-        handle_connection(stream);
+        match stream {
+            Ok(unwrapped_stream) => {
+                handle_connection(unwrapped_stream);
+            }
+            Err(e) => {
+                println!("{}", e.to_string());
+            }
+        }
     }
 }
 
@@ -18,9 +23,22 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
 
-    let contents = fs::read_to_string("index.html").unwrap();
-    
-    let response = format!("HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}", contents.len(), contents);
+    let get = b"GET / HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "index.html")
+    } else {
+        ("HTTP/1.1 404 Not Found", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    );
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
